b48bed8c715d5363e362dc0bfe154d03474fb49c
VidhubalaMatthuvarajan <vidhubala.m1@gmail.com>
2017-08-15 21:30:05 +0530

    Update Positive.java

--- Positive.java
+++ Positive.java
@@ -6,8 +6,12 @@ public static void main(String []args)
 Scanner sc=new Scanner(System.in);
 String s=sc.next();
 int n=Integer.parseInt(s);
-if(n>1)
+if(n>0&&n<=99)
 {
+  System.out.print(n);
+}
+  if(n>99)
+  {
 System.out.print(s.charAt(s.length()-2)+s.charAt(s.length()-1));
 }
 else
