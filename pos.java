02a5033c6a4af087713f02b225748d9f8a7f5ed5
VidhubalaMatthuvarajan <vidhubala.m1@gmail.com>
2017-09-01 16:24:29 +0530

    Create pos.java

---
+++ pos.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class pos
+{
+public static void main(String []args)
+{
+Scanner sc=new Scanner(System.in);
+int a=sc.nextInt();
+int d=0;
+if(a>=0&&a<=100000)
+{
+d++;
+}
+if(d==1&&a<0)
+{
+System.out.print("Negative");
+}
+if(d==1&&a>0)
+{
+System.out.print("Positive");
+}
+if(d==1&&a==0)
+{
+System.out.print("zero");
+}
+if(d==0)
+{
+System.out.print("Not in range");
+}
